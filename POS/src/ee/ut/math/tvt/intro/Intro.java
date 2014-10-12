f2cf1aa5d6b59137834eeb7b3c91f90f291bf7ef
Keity Eres <keity.eres@gmail.com>
2014-10-12 13:21:04 +0300

    Lisasin log4j jaoks vajalikud asjad

--- POS/src/ee/ut/math/tvt/intro/Intro.java
+++ POS/src/ee/ut/math/tvt/intro/Intro.java
@@ -1,12 +1,14 @@
 package ee.ut.math.tvt.intro;
 
+import org.apache.log4j.Logger;
+
 import ee.ut.math.tvt.intro.domain.controller.IntroDomainController;
 import ee.ut.math.tvt.intro.domain.controller.impl.IntroDomainControllerImpl;
 import ee.ut.math.tvt.intro.ui.IntroUi;
 
 
 public class Intro {
-	
+	private static final Logger log = Logger.getLogger(Intro.class);
 	public static void main(String args[]) {
 		
 		final IntroDomainController introController = new IntroDomainControllerImpl();
@@ -15,5 +17,6 @@ public class Intro {
 		final IntroUi ui = new IntroUi(introController);
 		
 		ui.setVisible(true);
+		log.info("Intro window is opened");
 	}
 }
